1555a3fb0da3a581af1e99a53126bd8966e84f1d
saiharshavarma <saiharshavarma@outlook.com>
2024-10-23 01:40:44 -0400

    fixed the homepage bug

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,22 @@
 <template>
-    <div class="flex flex-col p-8 justify-center">
-        <Meals :meals="meals" />  
-    </div>
+  <div class="flex flex-col p-8 justify-center">
+    <Meals :meals="meals" />
+  </div>
 </template>
 
 <script setup>
-import { computed, onMounted, ref } from 'vue';
-import store from '../store';
-import axiosClient from '../axiosClient.js'
-import Meals from "../components/Meals.vue";
+import { onMounted, ref } from 'vue';
+import axiosClient from '../axiosClient.js';
+import Meals from '../components/Meals.vue';
 
 const meals = ref([]);
 
 onMounted(async () => {
-  for (let i = 0; i < 12; i++) {
-    axiosClient
-      .get(`random.php`)
-      .then(({ data }) => meals.value.push(data.meals[0]));
+  try {
+    const { data } = await axiosClient.get('search.php?s=');
+    meals.value = data.meals.slice(0, 12);
+  } catch (error) {
+    console.error('Error fetching meals:', error);
   }
 });
 </script>
